433d24cc9f34332002f32459d8f1bfcc9796baf9
SUNAL PAL <sunal@SUNAL.com>
2025-09-08 19:23:02 +0530

    initial project setup with folder structure(frontend build files are copy in the public folder)

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express=require("express")
 const cookieparser=require("cookie-parser")
 const cors=require("cors")
+const path=require("path")
 
 // Routes
 const authRoutes=require("./routes/auth.routes")
@@ -19,8 +20,14 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieparser())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'./public')))
 
 // Using Routes
 app.use("/api/auth",authRoutes)
 app.use("/api/chat",chatRoutes)
+
+app.get("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/index.html'))
+})
+
 module.exports=app
