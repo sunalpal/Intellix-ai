6c1b80bd37a84a0060da9cf7f755af95cc8f8222
SUNAL PAL <sunal@SUNAL.com>
2025-09-12 20:27:39 +0530

    Update server.js to use environment variable for port configuration

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,10 @@ connectDB();
 
 initSocketServer(httpserver)
 
-httpserver.listen(3000,()=>{
-    console.log("Server running at port 3000");
-    
-})
+
+
+const PORT = process.env.PORT || 5000;
+
+httpserver.listen(PORT, () => {
+  console.log(` Server running on port ${PORT}`);
+});
